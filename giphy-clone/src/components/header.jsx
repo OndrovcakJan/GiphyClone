52157ef31b92e6cf4040f6dd439b0a2dd4fa2588
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-22 11:29:34 +0200

    zákl. header

--- giphy-clone/src/components/header.jsx
+++ giphy-clone/src/components/header.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+    const [showCategories, setShowCategories] = useState(false);
+
   return (
-    <div>Header</div>
+    <nav>
+        <div>
+            <Link to="/" className="flex gap-2">
+                <img src="/logo.svg" className="w-8 h-8" alt="Giphy Logo" />
+                <h1 className="text-5xl font-bold tracking-tight cursor-pointer">Giphy</h1>
+            </Link>
+
+    {/* render categories dropdown */}
+
+
+
+        </div>
+        </nav>
   )
 }
 
