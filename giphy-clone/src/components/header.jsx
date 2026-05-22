f126d0aa87623da0952d8aa4105dde29764685a2
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-22 11:44:38 +0200

    oprava: header: kolize mezi novým a starým tailwindem

--- giphy-clone/src/components/header.jsx
+++ giphy-clone/src/components/header.jsx
@@ -8,14 +8,15 @@ const Header = () => {
 
   return (
     <nav>
-        <div>
+        <div className="relative flex gap-4 justify-between items-center mb-2">
             <Link to="/" className="flex gap-2">
-                <img src="/logo.svg" className="w-8 h-8" alt="Giphy Logo" />
+                <img src="/logo.svg" className="w-8" alt="Giphy Logo" />
                 <h1 className="text-5xl font-bold tracking-tight cursor-pointer">Giphy</h1>
             </Link>
 
     {/* render categories dropdown */}
-
+    <Link className="px-4 py-1 border-b-4 hidden lg:block hover:bg-gradient-to-r hover:from-purple-600 hover:via-blue-600 hover:to-pink-600">
+    Reactions</Link>
 
 
         </div>
