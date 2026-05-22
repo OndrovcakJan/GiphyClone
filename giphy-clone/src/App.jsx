c738272e812406b4c1bf0b12dcb3b6e4e0dbcc84
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-22 10:32:26 +0200

    add routing setup and create AppLayout and Home components

--- giphy-clone/src/App.jsx
+++ giphy-clone/src/App.jsx
@@ -1,5 +1,30 @@
-
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import AppLayout from './layouts/app-layout'
+
+//homepage
+//categories
+//search
+//single gif page
+//favorites
+
+
+const router = createBrowserRouter([
+  {
+
+    element: <AppLayout />,
+
+    children:[
+      {
+        path: '/',
+
+        element: <Home />
+      }
+    ]
+
+
+  }
+])
 
 function App() {
   return (<h1 className="text-4xl font-bold text-blue-500">
