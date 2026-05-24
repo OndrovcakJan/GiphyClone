a3043637aeb6e93159fe7f5997f1bd9b9b77c7c4
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-24 20:28:14 +0200

    vyhledat a vymazat buttons

--- giphy-clone/src/components/gif-search.jsx
+++ giphy-clone/src/components/gif-search.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { HiMiniXMark, HiOutlineMagnifyingGlass } from "react-icons/hi2";
 
 const GifSearch = () => {
   const [query, setQuery] = useState("");
@@ -18,10 +19,24 @@ const GifSearch = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search for GIFs and Stickers"
-        className="w-full pl-4 pr-14 py-3 text-lg text-gray-800 bg-white rounded-lg border border-gray-300 outline-none placeholder-gray-400 shadow-sm focus:ring-2 "
+        className="w-full pl-4 pr-14 py-3 text-lg text-gray-800 bg-white border border-gray-300 outline-none placeholder-gray-400 shadow-sm focus:ring-2 "
       />
 
-      
+      {query && (
+        <button
+          onClick={() => setQuery("")}
+          className="absolute bg-gray-300 opacity-90 rounded-full right-20 mr-2 top-3.5 cursor-pointer p-1"
+        >
+          <HiMiniXMark size={22} />
+        </button>
+      )}
+
+      <button
+        onClick={searchGIFs}
+        className="bg-linear-to-tr from-pink-600 to-pink-400 text-white px-4 py-2 rounded-tr rounded-br cursor-pointer"
+      >
+        <HiOutlineMagnifyingGlass size={35} className="-scale-x-100" />
+      </button>
     </div>
   );
 };
